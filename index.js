6c8ed91a83f4a29691c3e9de875dcf8538767c96
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-06-08 23:09:08 +0300

    commit tree f29f9e9737495f074007828d07551481c4dd2320

---
+++ index.js
@@ -0,0 +1,9 @@
+function  receivesAFunction(cb){
+   return cb()
+}
+function  returnsANamedFunction(){
+    return function Dennis(){return "I wanna be the best programmer"}
+}
+function returnsAnAnonymousFunction(){
+    return function(){return "Greatness never come on a silver plate"}
+}
